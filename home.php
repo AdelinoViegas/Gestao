e3e75acbf56d72f77cf63b6341f8534348d7380b
AdelinoViegas <buchoteclas@gmail.com>
2025-03-03 20:13:41 +0100

    arrumando os arquivos

--- home.php
+++ home.php
@@ -1,192 +1,188 @@
-<?php 
+<?php
 require_once "conexao.php";
 session_start();
 
 $sql = "SELECT * FROM teste";
-$result = mysqli_query($conexao,$sql);
+$result = mysqli_query($conexao, $sql);
 
 //verficar se está logado
-if(!isset($_SESSION['logado'])){
+if (!isset($_SESSION['logado'])) {
   header("Location: index.php");
 }
 
 ?>
-
 <!DOCTYPE html>
-<html>
+<html lang="pt">
 <head>
-	<title>Professor</title>
-  <?php require_once "head.php";  ?>
+  <title>Professor</title>
+  <?php require_once "head.php"; ?>
 </head>
+
 <body>
-	<div class="divsuperior">
-	  <h1>Colégio Samiga</h1>
-    </div>
+  <div class="divsuperior">
+    <h1>Colégio Samiga</h1>
+  </div>
 
-<!--Navebar-->
-<div class="navegacao">
-<ul>
-  <li class="list active">
-    <a href="home.php">
-      <span class="icon"><img src="img/home_white_24dp.svg"></span>
-      <span class="title">HOME</span>
-    </a>
-  </li>
-    <li class="list">
-    <a href="professor.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Professores</span>
-    </a>
-  </li>
-    <li class="list ">
-    <a href="aluno.php">
-      <span class="icon"><img src="img/school_white_24dp.svg"></span>
-      <span class="title">Alunos</span>
-    </a>
-  </li>
-    <li class="list ">
-    <a href="Encarregados.php">
-      <span class="icon"><img src="img/escalator_warning_white_24dp.svg"></span>
-      <span class="title">Encarregados</span>
-    </a>
-  </li>
-  <li class="list ">
-    <a href="usuarios.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Usuarios</span>
-    </a>
-    <li class="list ">
-    <a href="disciplinas.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Disciplinas</span>
-    </a>
-    <li class="list ">
-    <a href="turmas.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Turmas</span>
-    </a>
-    <li class="list ">
-    <a href="classes.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Classes</span>
-    </a>
-    <li class="list ">
-     <a href="grades.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Grades</span>
-    </a>
-    </li>
-    <li class="list">
-    <a href="pauta.php">
-      <span class="icon"><img src="img/format_list_numbered_white_24dp.svg"></span>
-      <span class="title">Pautas</span>
-    </a>
-  </li>
-    <li class="list">
-    <a href="logoult.php">
-      <span class="icon"><img src="img/logout_white_24dp.svg"></span>
-      <span class="title">Sair</span>
-    </a>
-  </li>
-</ul> 
-</div>
+  <!--Navebar-->
+  <div class="navegacao">
+    <ul>
+      <li class="list active">
+        <a href="home.php">
+          <span class="icon"><img src="img/home_white_24dp.svg"></span>
+          <span class="title">HOME</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="professor.php">
+          <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Professores</span>
+        </a>
+      </li>
+      <li class="list ">
+        <a href="aluno.php">
+          <span class="icon"><img src="img/school_white_24dp.svg"></span>
+          <span class="title">Alunos</span>
+        </a>
+      </li>
+      <li class="list ">
+        <a href="Encarregados.php">
+          <span class="icon"><img src="img/escalator_warning_white_24dp.svg"></span>
+          <span class="title">Encarregados</span>
+        </a>
+      </li>
+      <li class="list ">
+        <a href="usuarios.php">
+          <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Usuarios</span>
+        </a>
+      <li class="list ">
+        <a href="disciplinas.php">
+          <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Disciplinas</span>
+        </a>
+      <li class="list ">
+        <a href="turmas.php">
+          <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Turmas</span>
+        </a>
+      <li class="list ">
+        <a href="classes.php">
+          <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Classes</span>
+        </a>
+      <li class="list ">
+        <a href="grades.php">
+          <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Grades</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="pauta.php">
+          <span class="icon"><img src="img/format_list_numbered_white_24dp.svg"></span>
+          <span class="title">Pautas</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="logoult.php">
+          <span class="icon"><img src="img/logout_white_24dp.svg"></span>
+          <span class="title">Sair</span>
+        </a>
+      </li>
+    </ul>
+  </div>
 
-
-<!--Menu mobile-->    
-<nav class="navbar navbar-dark" id="navMenu">
+  <!--Menu mobile-->
+  <nav class="navbar navbar-dark" id="navMenu">
     <div class="container-fluid">
 
-    <button class="navbar-toggler" type="button"  data-bs-toggle="collapse" data-bs-target="#menu-mobile" aria-expanded="true" id="menuMobile">
-        <span class="navbar-toggler-icon" ></span>
-    </button>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu-mobile"
+        aria-expanded="true" id="menuMobile">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="navbar-collapse collapse" id="menu-mobile">
+      <div class="navbar-collapse collapse" id="menu-mobile">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="home.php">
-                 <span class="icon"><img src="img/home_white_24dp.svg"></span>
-           <span class="title">HOME</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="professor.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Professores</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="aluno.php">
-               <span class="icon"><img src="img/school_white_24dp.svg"></span>
-           <span class="title">Alunos</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="encarregados.php">
-            <span class="icon"><img src="img/escalator_warning_white_24dp.svg"></span>
-            <span class="title">Encarregados</span>
-                </a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="usuarios.php">
-                  <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-                  <span class="title">Usuarios</span>
-                </a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="disciplinas.php">
-                 <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-                 <span class="title">Disciplinas</span>
-                </a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="turmas.php">
-             <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-                 <span class="title">Turmas</span>
-                </a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="classes.php">
-                  <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-                  <span class="title">Classes</span>
-                </a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="grades.php">
-                 <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-                 <span class="title">Grades</span>
-                </a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="pautas.php">
-               <span class="icon"><img src="img/format_list_numbered_white_24dp.svg"></span>
-            <span class="title">Pautas</span>
-                </a>
-            </li>
-             <li class="nav-item">
-                <a class="nav-link" id="opcMobile" href="logoult.php">
-            <span class="icon"><img src="img/logout_white_24dp.svg"></span>
-            <span class="title">Sair</span>
-                </a>
-            </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="home.php">
+              <span class="icon"><img src="img/home_white_24dp.svg"></span>
+              <span class="title">HOME</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="professor.php">
+              <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+              <span class="title">Professores</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="aluno.php">
+              <span class="icon"><img src="img/school_white_24dp.svg"></span>
+              <span class="title">Alunos</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="encarregados.php">
+              <span class="icon"><img src="img/escalator_warning_white_24dp.svg"></span>
+              <span class="title">Encarregados</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="usuarios.php">
+              <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+              <span class="title">Usuarios</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="disciplinas.php">
+              <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+              <span class="title">Disciplinas</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="turmas.php">
+              <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+              <span class="title">Turmas</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="classes.php">
+              <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+              <span class="title">Classes</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="grades.php">
+              <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+              <span class="title">Grades</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="pautas.php">
+              <span class="icon"><img src="img/format_list_numbered_white_24dp.svg"></span>
+              <span class="title">Pautas</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="opcMobile" href="logoult.php">
+              <span class="icon"><img src="img/logout_white_24dp.svg"></span>
+              <span class="title">Sair</span>
+            </a>
+          </li>
 
 
         </ul>
+      </div>
     </div>
+  </nav>
 
-    </div>
- </nav>
+  <div class="alerta">
+    <p>Bem Vindo ao sistema Sr.
+      <?php $nome = $_SESSION['painel'];
+      echo "<h1>$nome</h1>";
+      ?>
+    </p>
+  </div>
 
-
-
-   <div class="alerta">
-    	<p>Bem Vindo ao sistema Sr.
-    		<?php $nome = $_SESSION['painel'];
-    		   echo "<h1>$nome</h1>";
-    		?>
-    	</p>
-    </div>               
-
-
-<?php require_once "footer.php";  ?>
+  <?php require_once "footer.php"; ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,97 +1,99 @@
 <?php
-
-//conexão
 require_once "conexao.php";
-//Sessão
 session_start();
 
-   if(isset($_POST['enviar-dados']) ){
+if (isset($_POST['enviar-dados'])) {
+  $erros = array();
+  //Filtrando os valores do login e senha para as variaveis $login e $senha
 
-      $erros = array();
-      //Filtrando os valores do login e senha para as variaveis $login e $senha
+  $login = mysqli_escape_string(
+    $conexao,
+    $_POST['txtnome']
+  );
+  $senha = mysqli_escape_string(
+    $conexao,
+    $_POST['txtsenha']
+  );
+  $painel = mysqli_escape_string(
+    $conexao,
+    $_POST['selecao']
+  );
 
-      $login = mysqli_escape_string($conexao,
-        $_POST['txtnome']);
-      $senha = mysqli_escape_string($conexao,
-        $_POST['txtsenha']);
-      $painel = mysqli_escape_string($conexao,
-        $_POST['selecao']);
-
-       $conection = $ligation->prepare("SELECT * FROM sg_usuarios WHERE nome_u = '$login'");
-       $conection->execute();
-       $array_usuario = $conection->fetchall(PDO::FETCH_ASSOC);
-       $array_usuario = $array_usuario[0];
+  $conection = $ligation->prepare("SELECT * FROM sg_usuarios WHERE nome_u = '$login'");
+  $conection->execute();
+  $array_usuario = $conection->fetchall(PDO::FETCH_ASSOC);
+  $array_usuario = $array_usuario[0];
 
 
-      if (count($array_usuario)  > 0){
-          //$v = mysqli_fetch_assoc($valo); 
-            $estado = $array_usuario['senha_u'];  
+  if (count($array_usuario) > 0) {
+    //$v = mysqli_fetch_assoc($valo); 
+    $estado = $array_usuario['senha_u'];
 
-      if(password_verify($senha,$estado)){
-          $senha = $estado;
-      }
+    if (password_verify($senha, $estado)) {
+      $senha = $estado;
     }
+  }
 
-      $sql = "SELECT * FROM sg_usuarios WHERE nome_u = '$login' AND senha_u = '$senha' AND estado_u ='activo' AND painel_u = '$painel'";
-      $res = $ligation->prepare($sql);
-      $res->execute();     
-      $dados =  $res->fetchall(PDO::FETCH_ASSOC);
-      $dados = $dados[0];
-      if(empty($login) || empty($senha)){
-        $erros[] = "<span>O campo login e senha preecisa ser preenchido</span>";
-      }else{
-       if (empty($dados)) {
-            $erros[] = "<span>Usuário enexistente</span>";
-        }else {
-               if($painel === 'admin'){           
-                if($dados['senha_u'] === $senha && $dados['nome_u'] === $login){                       
-                        $_SESSION['logado'] = true;
-                        $_SESSION['id_adm'] = $dados['id_u'];
-                        $_SESSION['nome'] = $dados['painel_u'];  
-                  header('Location: menu-home.php');
-                }
-              }elseif($painel === 'professor'){           
-                if($dados['senha_u'] === $senha && $dados['nome_u'] === $login){
-                  $id = $dados['id_u'];
-                  $v1 = $ligation->prepare("SELECT id_p FROM sg_professor WHERE idUsuario = '$id'");
-                  $v1->execute();
-                  $vect = $v1->fetchAll(PDO::FETCH_ASSOC);
-                  $vect = $vect[0];             
-                    $_SESSION['logado'] = true;
-                     $_SESSION['idp'] = $vect['id_p'];
-                     $_SESSION['iduser_p'] = $dados['id_u'];
-                    $_SESSION['nome_professor'] = $dados['nome_u'];  
-                  header('Location: professor/homeprof.php');
-                }
-              }elseif($painel === 'encarregado'){           
-                if($dados['senha_u'] === $senha && $dados['nome_u'] === $login){
-                  $id = $dados['id_u'];
-                  $v1 = $ligation->prepare("SELECT id_e FROM sg_encarregado WHERE idUsuario = '$id'");
-                   $v1->execute();
-                   $vect = $v1->fetchAll(PDO::FETCH_ASSOC);
-                   $vect = $vect[0];
-        
-                    $_SESSION['logado'] = true;
-                     $_SESSION['ide'] = $vect['id_e'];
-                     $_SESSION['iduser_e'] = $dados['id_u'];
-                    $_SESSION['nome_encarregado'] = $dados['nome_u'];  
-                  header('Location: encarregado/homepais.php');
-                }
-              }elseif($painel === 'aluno'){           
-                if($dados['senha_u'] === $senha && $dados['nome_u'] === $login){
-                  $id = $dados['id_u'];
-                  $v1 = $ligation->prepare("SELECT * FROM sg_aluno WHERE idUsuario = '$id'");
-                   $v1->execute();
-                   $vect = $v1->fetchAll(PDO::FETCH_ASSOC);
-                    $_SESSION['logado'] = true;
-                    $_SESSION['ida'] = $vect['id_a'];
-                    $_SESSION['iduser_a'] = $dados['id_u'];
-                    $_SESSION['nome_aluno'] = $dados['nome_u'];  
-                  header('Location: aluno/homealuno.php');
-                }
-              }
+  $sql = "SELECT * FROM sg_usuarios WHERE nome_u = '$login' AND senha_u = '$senha' AND estado_u ='activo' AND painel_u = '$painel'";
+  $res = $ligation->prepare($sql);
+  $res->execute();
+  $dados = $res->fetchall(PDO::FETCH_ASSOC);
+  $dados = $dados[0];
+  if (empty($login) || empty($senha)) {
+    $erros[] = "<span>O campo login e senha preecisa ser preenchido</span>";
+  } else {
+    if (empty($dados)) {
+      $erros[] = "<span>Usuário enexistente</span>";
+    } else {
+      if ($painel === 'admin') {
+        if ($dados['senha_u'] === $senha && $dados['nome_u'] === $login) {
+          $_SESSION['logado'] = true;
+          $_SESSION['id_adm'] = $dados['id_u'];
+          $_SESSION['nome'] = $dados['painel_u'];
+          header('Location: menu-home.php');
+        }
+      } elseif ($painel === 'professor') {
+        if ($dados['senha_u'] === $senha && $dados['nome_u'] === $login) {
+          $id = $dados['id_u'];
+          $v1 = $ligation->prepare("SELECT id_p FROM sg_professor WHERE idUsuario = '$id'");
+          $v1->execute();
+          $vect = $v1->fetchAll(PDO::FETCH_ASSOC);
+          $vect = $vect[0];
+          $_SESSION['logado'] = true;
+          $_SESSION['idp'] = $vect['id_p'];
+          $_SESSION['iduser_p'] = $dados['id_u'];
+          $_SESSION['nome_professor'] = $dados['nome_u'];
+          header('Location: professor/homeprof.php');
+        }
+      } elseif ($painel === 'encarregado') {
+        if ($dados['senha_u'] === $senha && $dados['nome_u'] === $login) {
+          $id = $dados['id_u'];
+          $v1 = $ligation->prepare("SELECT id_e FROM sg_encarregado WHERE idUsuario = '$id'");
+          $v1->execute();
+          $vect = $v1->fetchAll(PDO::FETCH_ASSOC);
+          $vect = $vect[0];
+
+          $_SESSION['logado'] = true;
+          $_SESSION['ide'] = $vect['id_e'];
+          $_SESSION['iduser_e'] = $dados['id_u'];
+          $_SESSION['nome_encarregado'] = $dados['nome_u'];
+          header('Location: encarregado/homepais.php');
+        }
+      } elseif ($painel === 'aluno') {
+        if ($dados['senha_u'] === $senha && $dados['nome_u'] === $login) {
+          $id = $dados['id_u'];
+          $v1 = $ligation->prepare("SELECT * FROM sg_aluno WHERE idUsuario = '$id'");
+          $v1->execute();
+          $vect = $v1->fetchAll(PDO::FETCH_ASSOC);
+          $_SESSION['logado'] = true;
+          $_SESSION['ida'] = $vect['id_a'];
+          $_SESSION['iduser_a'] = $dados['id_u'];
+          $_SESSION['nome_aluno'] = $dados['nome_u'];
+          header('Location: aluno/homealuno.php');
         }
       }
+    }
+  }
 }
 ?>
 
@@ -106,46 +108,41 @@ session_start();
   <link rel="stylesheet" type="text/css" href="css/media.css?v=5">
 </head>
 <body>
-<div class="card" id="formlog">
+  <div class="card" id="formlog">
     <div id="txtlogin">Login</div>
-   <div class="card-body">
-    <div id="img">
-    <img src="img/logo/logo3.jpg">
-     </div>
+    <div class="card-body">
+      <div id="img">
+        <img src="img/logo/logo3.jpg">
+      </div>
       <?php
-        if(!empty($erros)){
-          foreach($erros as $value){
-            echo $value."<br>";
-          }
-        } 
-        ?>
+      if (!empty($erros)) {
+        foreach ($erros as $value) {
+          echo $value . "<br>";
+        }
+      }
+      ?>
       <form id="formulario" action="index.php" method="post">
-         <div class="form-group">
-           <input type="texy" id="textnome" class="form-control"
-        name="txtnome" placeholder="Nome do usuário">
-         </div>
-         <div class="form-group">
-           <input type="password" id="textsenha" class="form-control"
-        name="txtsenha" placeholder="Senha do usuário">
-         </div>
-         <div class="form-group">
+        <div class="form-group">
+          <input type="texy" id="textnome" class="form-control" name="txtnome" placeholder="Nome do usuário">
+        </div>
+        <div class="form-group">
+          <input type="password" id="textsenha" class="form-control" name="txtsenha" placeholder="Senha do usuário">
+        </div>
+        <div class="form-group">
           <label for="textusuario">Painel</label>
-         <select id="textusuario" class="input form-control"
-           name="selecao" required>
-              <option value="">Selecione o painel</option>
-              <option value="aluno">Aluno</option>
-              <option value="admin">Administrador</option>
-              <option value="professor">Professor</option>
-              <option value="encarregado">Encarregado</option>
-           </select>
-         </div>
-         <button type="submit" class="btn btn-outline-primary btn-block" 
-         name="enviar-dados">Entrar</button>
+          <select id="textusuario" class="input form-control" name="selecao" required>
+            <option value="">Selecione o painel</option>
+            <option value="aluno">Aluno</option>
+            <option value="admin">Administrador</option>
+            <option value="professor">Professor</option>
+            <option value="encarregado">Encarregado</option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-outline-primary btn-block" name="enviar-dados">Entrar</button>
       </form>
-   </div>
-</div>
+    </div>
+  </div>
 
-<script src="bootstrap5/js/bootstrap.bundle.min.js"></script>
-
+  <script src="bootstrap5/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
