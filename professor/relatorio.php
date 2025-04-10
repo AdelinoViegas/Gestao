c9ba17e0fdaf6f5dd1caba9a7a9be1b94d51154e
AdelinoViegas <buchoteclas@gmail.com>
2025-04-09 22:43:36 -0300

    actualizando arquivos

---
+++ professor/relatorio.php
@@ -0,0 +1,30 @@
+<?php 
+
+include 'pdf-php/src/Cezpdf.php'; // Or use 'vendor/autoload.php' when installed through composer
+
+// Initialize a ROS PDF class object using DIN-A4, with background color gray
+$pdf = new Cezpdf('a4','portrait','color',[255,255,255]);
+// Set pdf Bleedbox
+$pdf->ezSetMargins(20,20,20,20);
+// Use one of the pdf core fonts
+$mainFont = 'Times-Roman';
+// Select the font
+$pdf->selectFont($mainFont);
+// Define the font size
+$size=13;
+// Modified to use the local file if it can
+$pdf->openHere('Fit');
+
+// Output some colored text by using text directives and justify it to the right of the document
+$pdf->ezText("Republica de Angola", $size, ['justification'=>'center']);
+$pdf->ezText("Governo da Provincia de Angola", $size, ['justification'=>'center']);
+$pdf->ezText("Administracao Municipal do Cazenga", $size, ['justification'=>'center']);
+$pdf->ezText("Direção da Educação", $size, ['justification'=>'center']);
+$pdf->ezText("-------------------------------------//-----------------------------------", $size, ['justification'=>'center']);
+
+$pdf->ezText(, $size, ['justification'=>'center']);
+// Output the pdf as stream, but uncompress
+$pdf->ezStream(['compress'=>0]);
+
+?>
+
