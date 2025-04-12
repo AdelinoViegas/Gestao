59ab1d92a16b4fb787348cd2464563e9f2cad05b
AdelinoViegas <buchoteclas@gmail.com>
2025-04-11 21:09:48 -0300

    actualizando menu-home.php

--- menu-home.php
+++ menu-home.php
@@ -1,113 +1,108 @@
-<?php 
+<?php
 require_once "conection.php";
 session_start();
 
 //verficar se está logado
-if(!isset($_SESSION['logado'])){
+if (!isset($_SESSION['logado'])) 
   header("Location: ../index.php");
-}
-
-//$sql = "SELECT * FROM teste";
-//$result = mysqli_query($conexao,$sql);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Samiga</title>
-  <?php require_once "head.php";  ?>
+  <title>Samiga</title>
+  <?php require_once "head.php"; ?>
 </head>
 <body>
-	<div class="divsuperior">
-	  <h1>Colégio Samiga</h1>
+  <div class="divsuperior">
+    <h1>Colégio Samiga</h1>
   </div>
   <div class="divsuperior2">
     <div class="divflex">
-    <div>
-      <h5>Painel-principal</h5>
-    </div>
-    <div class="d-flex">
-      <h5 class="me-2">Usuário :</h5>
-      <img class="me-1" src="img/person.svg" id="IMG">
-      <h5 class="me-3">Administrador</h5>
-    </div>
+      <div>
+        <h5>Painel-principal</h5>
+      </div>
+      <div class="d-flex">
+        <h5 class="me-2">Usuário :</h5>
+        <img class="me-1" src="img/person.svg" id="IMG">
+        <h5 class="me-3">Administrador</h5>
+      </div>
     </div>
   </div>
 
-<!--Navebar-->
-<div class="navegacao">
-<ul>
-  <li class="list active">
-    <a href="menu-home.php">
-      <span class="icon"><img src="img/home_white_24dp.svg"></span>
-      <span class="title">HOME</span>
-    </a>
-  </li>
-    <li class="list">
-    <a href="menu-professores.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Professores</span>
-    </a>
-  </li>
-  <li class="list ">
-    <a href="menu-Encarregados.php">
-      <span class="icon"><img src="img/escalator_warning_white_24dp.svg"></span>
-      <span class="title">Encarregados</span>
-    </a>
-  </li>
-    <li class="list">
-    <a href="menu-alunos.php">
-      <span class="icon"><img src="img/school_white_24dp.svg"></span>
-      <span class="title">Alunos</span>
-    </a>
-  </li>
-  <li class="list ">
-    <a href="menu-usuarios.php">
-      <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Usuarios</span>
-    </a>
-    <li class="list ">
-    <a href="menu-disciplinas.php">
-      <span class="icon"><img src="img/livro.png"></span>
-      <span class="title">Disciplinas</span>
-    </a>
-    <li class="list ">
-    <a href="menu-turmas.php">
-      <span class="icon"><img src="img/edit.png"></span>
-      <span class="title">Turmas</span>
-    </a>
-    <li class="list ">
-    <a href="menu-classes.php">
-      <span class="icon"><img src="img/edit.png"></span>
-      <span class="title">Classes</span>
-    </a>
-    <li class="list ">
-     <a href="menu-gerenciar.php">
-      <span class="icon"><img src="img/gerenciar.png"></span>
-      <span class="title">Gerenciamento</span>
-    </a>
-    </li>
-    <li class="list ">
-     <a href="menu-configurar.php">
-      <span class="icon"><img src="img/settings.png"></span>
-      <span class="title">Alterar-senha</span>
-    </a>
-    </li>
-    <li class="list">
-    <a href="logoult.php">
-      <span class="icon"><img src="img/logout_white_24dp.svg"></span>
-      <span class="title">Sair</span>
-    </a>
-  </li>
-</ul> 
-</div>
+  <!--Navebar-->
+  <div class="navegacao">
+    <ul>
+      <li class="list active">
+        <a href="menu-home.php">
+          <span class="icon"><img src="img/home_white_24dp.svg"></span>
+          <span class="title">HOME</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="menu-professores.php">
+          <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Professores</span>
+        </a>
+      </li>
+      <li class="list ">
+        <a href="menu-Encarregados.php">
+          <span class="icon"><img src="img/escalator_warning_white_24dp.svg"></span>
+          <span class="title">Encarregados</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="menu-alunos.php">
+          <span class="icon"><img src="img/school_white_24dp.svg"></span>
+          <span class="title">Alunos</span>
+        </a>
+      </li>
+      <li class="list ">
+        <a href="menu-usuarios.php">
+          <span class="icon"><img src="img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Usuarios</span>
+        </a>
+      <li class="list ">
+        <a href="menu-disciplinas.php">
+          <span class="icon"><img src="img/livro.png"></span>
+          <span class="title">Disciplinas</span>
+        </a>
+      <li class="list ">
+        <a href="menu-turmas.php">
+          <span class="icon"><img src="img/edit.png"></span>
+          <span class="title">Turmas</span>
+        </a>
+      <li class="list ">
+        <a href="menu-classes.php">
+          <span class="icon"><img src="img/edit.png"></span>
+          <span class="title">Classes</span>
+        </a>
+      <li class="list ">
+        <a href="menu-gerenciar.php">
+          <span class="icon"><img src="img/gerenciar.png"></span>
+          <span class="title">Gerenciamento</span>
+        </a>
+      </li>
+      <li class="list ">
+        <a href="menu-configurar.php">
+          <span class="icon"><img src="img/settings.png"></span>
+          <span class="title">Alterar-senha</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="logoult.php">
+          <span class="icon"><img src="img/logout_white_24dp.svg"></span>
+          <span class="title">Sair</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <?php require_once "navbarMobile.php"; ?>
 
-<?php require_once "navbarMobile.php";  ?>
+  <div id="imagem">
+    <img src="img/admin.png">
+  </div>
 
-<div id="imagem">
-  <img src="img/admin.png">
-</div>
-
-<?php require_once "footer.php";  ?>
+  <?php require_once "footer.php"; ?>
 </body>
 </html>
