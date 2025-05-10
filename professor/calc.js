71e71ce3ee5f1d541ee74e86468a3139cc140439
AdelinoViegas <buchoteclas@gmail.com>
2025-05-10 09:50:10 -0300

    retirando espaços desnecessários

--- professor/calc.js
+++ professor/calc.js
@@ -1,12 +1,11 @@
-      function calcular() {
-        
-          var av1 = Number(document.getElementById('aval1').value);
-          var av2 = Number(document.getElementById('aval2').value);
-          var av3 = Number(document.getElementById('aval3').value);
-          var mVal = document.getElementById('mediav').value = (av1 + av2 + av3)/3;
+function calcular() {
 
-         /* var p1 = Number(document.getElementById('prova1').value);
-          var p2 = Number(document.getElementById('prova2').value);
-          var mprova = document.getElementById('mediap').value = (p1 + p2)/2;*/
+  var av1 = Number(document.getElementById('aval1').value);
+  var av2 = Number(document.getElementById('aval2').value);
+  var av3 = Number(document.getElementById('aval3').value);
+  var mVal = document.getElementById('mediav').value = (av1 + av2 + av3) / 3;
 
-        }
+  /* var p1 = Number(document.getElementById('prova1').value);
+   var p2 = Number(document.getElementById('prova2').value);
+   var mprova = document.getElementById('mediap').value = (p1 + p2)/2;*/
+}
--- professor/notasAlunos.php
+++ professor/notasAlunos.php
@@ -19,7 +19,6 @@ if(isset($_POST['btn-update'])){
   header('Location: notas-editar.php');
 }
 
-
 ?>
 <!DOCTYPE html>
 <html>
