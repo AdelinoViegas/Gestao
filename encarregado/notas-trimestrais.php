403bf0d85e447346a19e4ad9158a78836ff5de33
AdelinoViegas <buchoteclas@gmail.com>
2025-05-10 10:49:49 -0300

    actualizando arquivos

--- encarregado/notas-trimestrais.php
+++ encarregado/notas-trimestrais.php
@@ -1,177 +1,165 @@
-<?php 
-require_once "../conexao.php";
-
+<?php
+require_once "../connection.php";
+require_once "../features/getData.php";
 session_start();
 
-//$estudante = $_SESSION['nome_aluno'];// id = 5;
-$id_estudante = $_POST['ID_aluno'];
-$trim_estudante = $_POST['trimA'];
+  $student_id = mysqli_real_escape_string($connection, trim($_SESSION['student_id']));
+  $quarter = mysqli_real_escape_string($connection, trim($_SESSION['quarter']));
 
+  $query = "SELECT * FROM sg_notas AS n JOIN sg_aluno AS a ON n.id_aluno = a.id_a JOIN sg_gerenciar AS g ON g.id_g = n.id_gerenciar JOIN sg_disciplina AS d ON d.id_d = g.idDisciplina WHERE id_aluno =? AND id_trimestre =?";
+  $data = getData($connection, $query, [$student_id, $quarter]);
 
-$def = "SELECT * FROM sg_notas AS n JOIN sg_aluno AS a ON n.id_aluno = a.id_a JOIN sg_gerenciar AS g ON g.id_g = n.id_gerenciar JOIN sg_disciplina AS d ON d.id_d = g.idDisciplina WHERE id_aluno = '$id_estudante' AND id_trimestre = '$trim_estudante'";
-$vet = mysqli_query($conexao,$def);
-
+  if (isset($_POST['btn-search'])) {
+    $search = mysqli_real_escape_string($connection, trim($_POST['search']));
+    $data = getData($connection, "SELECT * FROM sg_notas AS n JOIN sg_aluno AS a ON n.id_aluno = a.id_a JOIN sg_gerenciar AS g ON g.id_g = n.id_gerenciar JOIN sg_disciplina AS d ON d.id_d = g.idDisciplina WHERE id_aluno =? AND id_trimestre =? AND nome_d LIKE '$search%'", [$student_id, $quarter]);
+  }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Samiga</title>
-    <?php require_once "../head2.php"; ?>
+  <?php require_once "../head2.php"; ?>
 </head>
 <body>
-
   <div class="divsuperior">
     <h1>Colégio Samiga</h1>
   </div>
 
   <div class="divsuperior2">
     <div class="divflex">
-    <div>
-          <?php
-               
-       $sum = mysqli_query($conexao,"SELECT * FROM sg_aluno AS a JOIN sg_turma as t ON a.idTurma_a = t.id_t WHERE id_a = '$id_estudante'");
-       $trm = mysqli_fetch_assoc($sum);
-
-       echo "<h5 id='alinhar'>".$trim_estudante."º Trimestre </h5> <h5 id='alinhar'>      </h5><p id='fonte'>Turma:</p> <h5 id='alinhar'>".$trm['nome_t']."</h5> ";
-     ?>
-    </div>
-    <div class="d-flex">
-      <h5 class="me-2">Usuário :</h5>
-      <img class="me-1" src="../img/person.svg" id="IMG">
-      <h5 class="me-3">Encarregado</h5>
-    </div>
-    </div>
-  </div>
-
-<!--Navebar-->
-<div class="navegacao">
-<ul>
-  <li class="list">
-    <a href="homepais.php">
-      <span class="icon"><img src="../img/home_white_24dp.svg"></span>
-      <span class="title">HOME</span>
-    </a>
-  </li>
-  <li class="list active">
-    <a href="ver-notas.php">
-      <span class="icon"><img src="../img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Ver-notas</span>
-    </a>
-    </li>
-  <li class="list">
-    <a href="conf-encarregado.php">
-      <span class="icon"><img src="../img/settings.png"></span>
-      <span class="title">Alterar-senha</span>
-    </a>
-  </li>
-  <li class="list">
-    <a href="../logoult.php">
-      <span class="icon"><img src="../img/logout_white_24dp.svg"></span>
-      <span class="title">Sair</span>
-    </a>
-  </li>
-</ul> 
-</div>
-                   
-
-<?php require_once "navMob-encarregado.php";  ?>
-
-<div class="rounded-3" id="divm">
-  <div class="divsuperior3">
-    <?php
-
-       $sum = mysqli_query($conexao,"SELECT * FROM sg_turma AS t JOIN sg_aluno as a ON a.idTurma_a = t.id_t JOIN sg_classe as c ON  t.idClasse_t = c.id_c  WHERE id_a = '$id_estudante'");
-
-       $trm = mysqli_fetch_assoc($sum);
-        echo "<h5 id='alinhar'>".$trim_estudante."º Trimestre </h5> <h5 id='alinhar'>      </h5><p id='fonte'>Turma:</p> <h5 id='alinhar'>".$trm['nome_t']."</h5> ";
-
-     ?>
-  </div>
-
-<div id="divflex">
-    <button type="button" id="adicionar" class="btn btn-secondary"><strong id="fonte">
-    <?= $trm['nome_c']; ?>
-  </strong></button>
-
-<form action="" method="post">
-  <div id="btn-pesquisar">
-    <input type="text" class="form-control me-2" name="txtpesquisar" placeholder="Pesquisa a disciplina"><button id="btn-p" type="submit" class="btn btn-success" name="btn-pesquisa">Pesquisar</button>
-  </div>
-</form> 
-
-</div>
-
-<div class="table-responsive" id="tabdados">
-
-
-
-<table class="table table-hover table-bordered" id="table">
-
-  <thead class="table-secondary" id="theader">
-    <tr>
-      <th scope="col">Disciplina</th>
-      <th scope="col">Aval1</th>
-      <th scope="col">Aval2</th>
-      <th scope="col">Aval3</th>
-      <th scope="col">Media-Aval</th>
-      <th scope="col">Prova1</th>
-      <th scope="col">Prova2</th>
-      <th scope="col">Media-Prova</th>
-      <th scope="col">Media-Final</th>
-      <th scope="col">Classificação</th>
-    </tr>
-  </thead>
-  <tbody>
-      <?php 
-
-
-     
-      if(mysqli_num_rows($vet) > 0){
-      
-      while($estudante = mysqli_fetch_assoc($vet)) { 
-           
+      <div>
+        <?php
+        $group_data = getData($connection, "SELECT * FROM sg_aluno AS a JOIN sg_turma as t ON a.idTurma_a = t.id_t WHERE id_a =?", [$student_id])[0];
+        echo "<h5 id='alinhar'>" . $quarter . "º Trimestre </h5> <h5 id='alinhar'></h5><p id='fonte'>Turma:</p> <h5 id='alinhar'>" . $group_data['nome_t'] . "</h5> ";
         ?>
+      </div>
+      <div class="d-flex">
+        <h5 class="me-2">Usuário :</h5>
+        <img class="me-1" src="../img/person.svg" id="IMG">
+        <h5 class="me-3">Encarregado</h5>
+      </div>
+    </div>
+  </div>
 
-     <tr>    
-      <td><?= $estudante['nome_d']; ?></td>
-      <td><?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name = 'aval1' readonly value='".$estudante['avaliacao1']."'>" ?></td>
-      <td><?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name='aval2' readonly value='".$estudante['avaliacao2']."'>"  ?></td>
-      <td><?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name='aval3' readonly value='".$estudante['avaliacao3']."'>"  ?></td>
-      <td><?= "<input class='form-control ps-1' type='text' name='mediav' readonly value='".number_format($estudante['mediaAv'],2)."'>"  ?></td>
-      <td><?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name='prova1' readonly value='".$estudante['prova1']."'>"  ?></td>
-      <td><?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name='prova2' readonly value='".$estudante['prova2']."'>"  ?></td>
-      <td><?= "<input class='form-control ps-1' type='text' name='mediap' readonly value='".number_format($estudante['mediaPv'],2)."'>"  ?></td>
-      <td><?= "<input class='form-control ps-1' type='text' readonly value='".number_format($estudante['mediaF'],1)."'>"  ?>
-      </td>
-      <td><?= $estudante['classificacao']; ?>
-    </tr>
+  <div class="navegacao">
+    <ul>
+      <li class="list">
+        <a href="homepais.php">
+          <span class="icon"><img src="../img/home_white_24dp.svg"></span>
+          <span class="title">HOME</span>
+        </a>
+      </li>
+      <li class="list active">
+        <a href="ver-notas.php">
+          <span class="icon"><img src="../img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Ver-notas</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="conf-encarregado.php">
+          <span class="icon"><img src="../img/settings.png"></span>
+          <span class="title">Alterar-senha</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="../logoult.php">
+          <span class="icon"><img src="../img/logout_white_24dp.svg"></span>
+          <span class="title">Sair</span>
+        </a>
+      </li>
+    </ul>
+  </div>
 
-  <?php } ?>
+  <?php require_once "navMob-encarregado.php"; ?>
 
-  </tbody>
-</table>
+  <div class="rounded-3" id="divm">
+    <div class="divsuperior3">
+      <?php
+      $group_data = getData($connection, "SELECT * FROM sg_turma AS t JOIN sg_aluno as a ON a.idTurma_a = t.id_t JOIN sg_classe as c ON  t.idClasse_t = c.id_c  WHERE id_a =?", [$student_id])[0];
+      echo "<h5 id='alinhar'>" . $quarter . "º Trimestre </h5> <h5 id='alinhar'>      </h5><p id='fonte'>Turma:</p> <h5 id='alinhar'>" . $group_data['nome_t'] . "</h5> ";
+      ?>
+    </div>
 
-<?php
+    <div id="divflex">
+      <button type="button" id="adicionar" class="btn btn-secondary"><strong id="fonte">
+          <?= $group_data['nome_c']; ?>
+        </strong></button>
 
- }else{
+      <form action="" method="post">
+        <div id="btn-pesquisar">
+          <input type="text" class="form-control me-2" name="search" placeholder="Pesquisa a disciplina"><button
+            id="btn-p" type="submit" class="btn btn-success" name="btn-search">Pesquisar</button>
+        </div>
+      </form>
+    </div>
 
- ?> 
-   </tbody>
-</table> 
-  <tfooter class='text text-center'>    
-      <h5>Nenhum dado encontrado</h5>
-   </tfooter>
-<?php   
- }
-?>
+    <div class="table-responsive" id="tabdados">
+      <table class="table table-hover table-bordered" id="table">
+        <thead class="table-secondary" id="theader">
+          <tr>
+            <th scope="col">Disciplina</th>
+            <th scope="col">Aval1</th>
+            <th scope="col">Aval2</th>
+            <th scope="col">Aval3</th>
+            <th scope="col">Media-Aval</th>
+            <th scope="col">Prova1</th>
+            <th scope="col">Prova2</th>
+            <th scope="col">Media-Prova</th>
+            <th scope="col">Media-Final</th>
+            <th scope="col">Classificação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if (count($data) > 0) {
+            foreach ($data as $student) {
+              ?>
+              <tr>
+                <td><?= $student['nome_d']; ?></td>
+                <td>
+                  <?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name = 'aval1' readonly value='" . $student['avaliacao1'] . "'>" ?>
+                </td>
+                <td>
+                  <?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name='aval2' readonly value='" . $student['avaliacao2'] . "'>" ?>
+                </td>
+                <td>
+                  <?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name='aval3' readonly value='" . $student['avaliacao3'] . "'>" ?>
+                </td>
+                <td>
+                  <?= "<input class='form-control ps-1' type='text' name='mediav' readonly value='" . number_format($student['mediaAv'], 2) . "'>" ?>
+                </td>
+                <td>
+                  <?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name='prova1' readonly value='" . $student['prova1'] . "'>" ?>
+                </td>
+                <td>
+                  <?= "<input class='form-control ps-1' type='text' maxlength='4' size='2' name='prova2' readonly value='" . $student['prova2'] . "'>" ?>
+                </td>
+                <td>
+                  <?= "<input class='form-control ps-1' type='text' name='mediap' readonly value='" . number_format($student['mediaPv'], 2) . "'>" ?>
+                </td>
+                <td>
+                  <?= "<input class='form-control ps-1' type='text' readonly value='" . number_format($student['mediaF'], 1) . "'>" ?>
+                </td>
+                <td><?= $student['classificacao']; ?>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+        <?php
+          } else {
+            ?>
+        </tbody>
+        </table>
+        <tfooter class='text text-center'>
+          <h5>Nenhum dado encontrado</h5>
+        </tfooter>
+      <?php
+          }
+          ?>
+    </div>
+  </div>
 
-</div>
-
-
-</div>
-
-
-<?php require_once "../footer2.php";  ?>
-
+  <?php require_once "../footer2.php"; ?>
 </body>
 </html>
--- encarregado/ver-notas.php
+++ encarregado/ver-notas.php
@@ -1,129 +1,111 @@
-<?php 
-require_once "../conexao.php";
-
+<?php
+require_once "../connection.php";
+require_once "../features/getData.php";
 session_start();
 
-$id_e = $_SESSION['ide'];
-$nome = $_SESSION['nome_encarregado'];
+$responsible_id = $_SESSION['responsible_id'];
+$responsible_name = $_SESSION['responsible_name'];
 
-/*if(isset($_POST['ok'])){
-  
- $_SESSION['periodo'] = $_POST['trimestre'];
- $_SESSION['gerir'] = $_POST['gerenciamento'];
-  header('Location: notasAlunos.php');
-}*/
-
-
+if(isset($_POST['btn-notes'])){
+ $_SESSION['student_id'] = $_POST['student_id'];
+ $_SESSION['quarter'] = $_POST['quarter'];
+  header('Location: notas-trimestrais.php');
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
   <title>Samiga</title>
-    <?php require_once "../head2.php"; ?>
+  <?php require_once "../head2.php"; ?>
 </head>
 <body>
-
   <div class="divsuperior">
     <h1>Colégio Samiga</h1>
   </div>
 
   <div class="divsuperior2">
     <div class="divflex">
-    <div>
-      <h5>Ver-notas</h5>
-    </div>
-    <div class="d-flex">
-      <h5 class="me-2">Usuário :</h5>
-      <img class="me-1" src="../img/person.svg" id="IMG">
-      <h5 class="me-3">Encarregado</h5>
-    </div>
+      <div>
+        <h5>Ver-notas</h5>
+      </div>
+      <div class="d-flex">
+        <h5 class="me-2">Usuário :</h5>
+        <img class="me-1" src="../img/person.svg" id="IMG">
+        <h5 class="me-3">Encarregado</h5>
+      </div>
     </div>
   </div>
 
-<!--Navebar-->
-<div class="navegacao">
-<ul>
-  <li class="list">
-    <a href="homepais.php">
-      <span class="icon"><img src="../img/home_white_24dp.svg"></span>
-      <span class="title">HOME</span>
-    </a>
-  </li>
-  <li class="list active">
-    <a href="ver-notas.php">
-      <span class="icon"><img src="../img/perm_identity_white_24dp.svg"></span>
-      <span class="title">Ver-notas</span>
-    </a>
-    </li>
-  <li class="list">
-    <a href="conf-encarregado.php">
-      <span class="icon"><img src="../img/settings.png"></span>
-      <span class="title">Alterar-senha</span>
-    </a>
-  </li>
-  <li class="list">
-    <a href="../logoult.php">
-      <span class="icon"><img src="../img/logout_white_24dp.svg"></span>
-      <span class="title">Sair</span>
-    </a>
-  </li>
-</ul> 
-</div>
-                    
-
-<?php require_once "navMob-encarregado.php" ?>
-
-<div class="fontes rounded-3" id="divm">
-
-  <div class="divsuperior3">
-    <h5>Gerenciar</h5>
+  <div class="navegacao">
+    <ul>
+      <li class="list">
+        <a href="homepais.php">
+          <span class="icon"><img src="../img/home_white_24dp.svg"></span>
+          <span class="title">HOME</span>
+        </a>
+      </li>
+      <li class="list active">
+        <a href="ver-notas.php">
+          <span class="icon"><img src="../img/perm_identity_white_24dp.svg"></span>
+          <span class="title">Ver-notas</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="conf-encarregado.php">
+          <span class="icon"><img src="../img/settings.png"></span>
+          <span class="title">Alterar-senha</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="../logoult.php">
+          <span class="icon"><img src="../img/logout_white_24dp.svg"></span>
+          <span class="title">Sair</span>
+        </a>
+      </li>
+    </ul>
   </div>
 
-      <form action="notas-trimestrais.php" method="post">
+  <?php require_once "navMob-encarregado.php" ?>
 
-        <div class="row">
-         <div class="form-group col-md-4" id="margemB">
-            <label for="textdisciplina">Aluno</label>
-           <select id="textdisciplina" class="input form-control"
-           name="ID_aluno"  required>
-              <option value="">Selecione aqui</option>
+  <div class="fontes rounded-3" id="divm">
+    <div class="divsuperior3">
+      <h5>Gerenciar</h5>
+    </div>
 
-          <?php $query = mysqli_query($conexao,"SELECT a.nome_a,a.id_a,e.id_e FROM sg_aluno AS a INNER JOIN sg_encarregado AS e ON e.id_e = a.idEncarregado WHERE idEncarregado = '$id_e' "); 
+    <form action="" method="post">
+      <div class="row">
+        <div class="form-group col-md-4" id="margemB">
+          <label for="textdisciplina">Aluno</label>
+          <select id="textdisciplina" class="input form-control" name="student_id" required>
+            <option value="">Selecione aqui</option>
 
-          while ($registro = mysqli_fetch_assoc($query)) {
-              echo"<option value = '".$registro['id_a']."'>".$registro['nome_a']."</option>";
-  
-          }
-               
-          ?>    
-           </select>
-         </div>
+            <?php $query = getData($connection, "SELECT a.nome_a, a.id_a, e.id_e FROM sg_aluno AS a INNER JOIN sg_encarregado AS e ON e.id_e = a.idEncarregado WHERE idEncarregado =?", [$responsible_id]);
+            foreach ($query as $data)
+              echo "<option value = '" . $data['id_a'] . "'>" . $data['nome_a'] . "</option>";
+            ?>
+          </select>
+        </div>
 
         <div class="form-group col-md-4" id="margemB">
-            <label for="ttrimAluno">Trimestres</label>
-           <select id="ttrimAluno" class="input form-control"
-           name="trimA"  required>
-              <option value="">Selecione aqui</option>
-              <option value="1">1ª Trimestre</option>
-              <option value="2">2ª Trimestre</option>
-              <option value="3">3ª Trimestre</option>
-           </select>
-         </div>
-
-
-       <div class="col-md-2" id="margemB">
-        <label></label>
-          <button type="submit"  class="btn btn-success col-md-12" 
-           name="ok">Ver-notas</button>
+          <label for="ttrimAluno">Trimestres</label>
+          <select id="ttrimAluno" class="input form-control" name="quarter" required>
+            <option value="">Selecione aqui</option>
+            <option value="1">1ª Trimestre</option>
+            <option value="2">2ª Trimestre</option>
+            <option value="3">3ª Trimestre</option>
+          </select>
         </div>
-        
-        </div>
-      </form>
-</div>
 
-<?php require_once "../footer2.php";  ?>
+        <div class="col-md-2" id="margemB">
+          <label></label>
+          <button type="submit" class="btn btn-success col-md-12" name="btn-notes">Ver-notas</button>
+        </div>
+      </div>
+    </form>
+  </div>
+
+  <?php require_once "../footer2.php"; ?>
 
 </body>
-
 </html>
