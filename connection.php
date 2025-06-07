bff356c5c8eddcb5a4fb471e683e40f16a47398b
AdelinoViegas <buchoteclas@gmail.com>
2025-06-07 20:51:03 -0300

    alterando arquivos

--- connection.php
+++ connection.php
@@ -1,12 +1,6 @@
 <?php 
 try{
-  $nome_servidor = "localhost";
-  $nome_usuario = "root";
-  $palavra_passe = "";
-  $nome_banco_dados = "sg";
-
-  $connection = mysqli_connect($nome_servidor,
-  $nome_usuario,$palavra_passe,$nome_banco_dados);
+  $connection = mysqli_connect("localhost", "root", "", "sg");
 
   if(!$connection)
    throw new Exception("Falha ao se conectar". mysqli_connect_error());
